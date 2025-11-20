bc01d5ef40ab69b211108b13a3f919f5318099d5
Yuri Breion <yuribreion@gmail.com>
2025-11-20 22:35:47 +0100

    Improve and add more logs

--- api_client.py
+++ api_client.py
@@ -6,6 +6,7 @@ HTTP requests to GitHub
 import json
 import requests
 from requests.exceptions import HTTPError
+from logger import get_logger
 
 
 class APIClient:
@@ -16,8 +17,10 @@ class APIClient:
     def __init__(self, base_url, bearer_token):
         self.base_url = base_url
         self.bearer_token = bearer_token
-
-        print(f"GitHub HTTP Client started to connect with: {self.base_url}")
+        self.logger = get_logger(name="APIClient")
+        self.logger.info(
+            "GitHub HTTP Client started to connect with: %s", self.base_url
+        )
 
     def get(self, endpoint):
         """
@@ -37,10 +40,15 @@ class APIClient:
         try:
             response = requests.get(**requests_args)
             response.raise_for_status()
+            self.logger.info(
+                "Successful GET request to %s - Status %s",
+                full_url,
+                response.status_code,
+            )
             return json.dumps(response.json(), indent=4)
         except HTTPError as http_err:
-            print(f"HTTP Error occurred: {http_err}")
+            self.logger.error("HTTP Error occurred: %s", http_err)
             if http_err.response is not None:
-                print(f"Status: {http_err.response.status_code}")
-                print(f"Server Response: {http_err.response.text}")
+                self.logger.error("Status: %s", http_err.response.status_code)
+                self.logger.error("Server Response: %s", http_err.response.text)
             return None
